2f1e5f34d62dd298456f4ccaa50aacf6bdf29453
Eric-Edward <EricEdward.Pro@gmail.com>
2021-11-24 23:32:01 +0800

    方法说明。

--- inc/handleRequestPath.h
+++ inc/handleRequestPath.h
@@ -7,11 +7,22 @@
 
 #include <string>
 
+///
+/// \param reqUrl 从request请求中获得的请求地址。
+/// \return 返回具体的请求地址。
 std::string convertPath(char *reqUrl);
 
+
+///
+/// \param convertUrl 从上述函数中获取的具体地址。
+/// \return 返回本地资源的地址。
 std::string concatPath(std::string convertUrl);
 
 
+///
+/// \param reqUrl 输入request请求
+/// \return 获取本地的资源
+/// \brief 解析地址的总调用
 std::string serviceOfConvert(char *reqUrl);
 
 
--- src/handleRequestPath.cpp
+++ src/handleRequestPath.cpp
@@ -3,8 +3,14 @@
 //
 #include "../inc/handleRequestPath.h"
 
+///设置一个本地资源的基础路径。
 std::string SOURCE_URL = "D:\\homework\\electroniccommerce\\WebServer\\";
 
+
+///
+/// \author Eric
+/// \details 根据request请求的格式得从第4位至之后的第一个空格为请求地址。所以设置一个string用来获取这一部分的内容。
+/// \attention 如果请求为 “/” ，则应该自动对应至index.html。
 std::string convertPath(char *reqUrl) {
     int start = 4;
     int end = start + 1;
